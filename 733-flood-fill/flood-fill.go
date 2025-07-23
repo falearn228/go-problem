712aa873ba540b6c7279a88420bf875aad76e847
falearn <127325061+falearn228@users.noreply.github.com>
2025-07-23 15:45:49 +0700

    Time: 0 ms (100.00%) | Memory: 5.6 MB (36.92%) - LeetSync

---
+++ 733-flood-fill/flood-fill.go
@@ -0,0 +1,42 @@
+func floodFill(image [][]int, sr int, sc int, color int) [][]int {
+	currColor := image[sr][sc]
+	if currColor == color {
+		return image
+	}
+	queue := make([][2]int, 0)
+	visited := make(map[[2]int]bool, 0)
+	queue = append(queue, [2]int{sr, sc})
+
+	rows, cols := len(image), len(image[0])
+	for len(queue) > 0 {
+		top := queue[0]
+		queue = queue[1:]
+
+		if flag := visited[top]; flag {
+			continue
+		}
+		visited[top] = true
+		row, col := top[0], top[1]
+		if row < 0 || row >= rows || col < 0 || col >= cols || image[row][col] != currColor {
+			continue
+		}
+		image[row][col] = color
+
+		neighbors := [][2]int{
+			{row - 1, col}, // вверх
+			{row + 1, col}, // вниз
+			{row, col - 1}, // влево
+			{row, col + 1}, // вправо
+		}
+
+		for _, neighbor := range neighbors {
+			nr, nc := neighbor[0], neighbor[1]
+			// Проверяем границы и что еще не посещали
+			if nr >= 0 && nr < rows && nc >= 0 && nc < cols && !visited[neighbor] {
+				queue = append(queue, neighbor)
+			}
+		}
+	}
+
+	return image
+}
