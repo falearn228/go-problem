0af635b4a77ac3091fdd27fd7228813a362d6c05
falearn <127325061+falearn228@users.noreply.github.com>
2025-07-31 23:38:44 +0700

    Time: 0 ms (100.00%) | Memory: 4.5 MB (29.80%) - LeetSync

---
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.go
@@ -0,0 +1,30 @@
+func gcdOfStrings(str1 string, str2 string) string {
+	if str1 + str2 != str2 + str1 {
+        return ""
+    }
+    maxSubStrLen := gcd(len(str1), len(str2))
+
+    return str1[:maxSubStrLen]
+}
+
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func gcd(a, b int) int {
+	for b != 0 {
+		a, b = b, a%b
+	}
+	return a
+}
