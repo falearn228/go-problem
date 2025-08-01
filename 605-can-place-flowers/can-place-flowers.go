a5a14d2f9a83a9e24fd93a7b5e50885a4f254df0
falearn <127325061+falearn228@users.noreply.github.com>
2025-08-01 14:44:00 +0700

    Time: 0 ms (100.00%) | Memory: 8.1 MB (53.96%) - LeetSync

---
+++ 605-can-place-flowers/can-place-flowers.go
@@ -0,0 +1,30 @@
+func canPlaceFlowers(flowerbed []int, n int) bool {
+    if n == 0 {
+        return true
+    }
+    if len(flowerbed) == 1 && n == 1 {
+        if flowerbed[0] == 1 {
+            return false
+        }
+        return true
+    }
+	for i, v := range flowerbed {
+		if v == 0 {
+			if i == 0 && flowerbed[i+1] != 1 {
+				flowerbed[i] = 1
+				n--
+			} else if i == len(flowerbed)-1 && flowerbed[i-1] != 1 {
+				flowerbed[i] = 1
+				n--
+
+			} else if i > 0 && i < len(flowerbed) - 1 && flowerbed[i-1] != 1 && flowerbed[i+1] != 1 {
+				flowerbed[i] = 1
+				n--
+			}
+		}
+		if n == 0 {
+			return true
+		}
+	}
+	return false
+}
