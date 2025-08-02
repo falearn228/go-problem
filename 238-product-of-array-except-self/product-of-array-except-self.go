8346efeb12169d8931e045e51fd53673ee07754c
falearn <127325061+falearn228@users.noreply.github.com>
2025-08-03 05:04:53 +0700

    Time: 15 ms (5.09%) | Memory: 9.7 MB (86.18%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.go
+++ 238-product-of-array-except-self/product-of-array-except-self.go
@@ -1,21 +1,22 @@
 func productExceptSelf(nums []int) []int {
-    prefixes := make([]int, len(nums))
-    sufixes := make([]int, len(nums))
-    n := len(nums)
-    prefixes[0] = 1
-    sufixes[n-1] = 1
+	n := len(nums)
+	answer := make([]int, len(nums))
+	answer[0] = 1
 
-    i, j := 1, n-2
-    for i < n && j >= 0 {
-        prefixes[i] = prefixes[i-1] * nums[i-1]
-        sufixes[j] = sufixes[j+1] * nums[j+1]
-        i++
-        j--
-    }
+	for i := range answer {
+		if i > 0 {
+			answer[i] = answer[i-1] * nums[i-1]
+		}
+	}
 
-    answer := make([]int, len(nums))
-    for i := range nums {
-        answer[i] = prefixes[i] * sufixes[i]
-    }
-    return answer
+	//sufx[i] = sufx[i+1] * nums[i+1]
+	i := n - 2
+	sufx := nums[n-1]
+	fmt.Println(answer)
+	for i >= 0 {
+		answer[i] *= sufx
+		sufx = sufx * nums[i]
+		i--
+	}
+	return answer
 }
