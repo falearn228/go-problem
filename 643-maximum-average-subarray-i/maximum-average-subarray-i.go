333d8607ba5e0905001a7cc65bc11839001b7d41
falearn <127325061+falearn228@users.noreply.github.com>
2025-08-06 02:00:09 +0700

    Time: 0 ms (100.00%) | Memory: 12.8 MB (12.59%) - LeetSync

---
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.go
@@ -0,0 +1,18 @@
+func findMaxAverage(nums []int, k int) float64 {
+    maxAvg := math.Inf(-1)
+    maxSum := 0
+    for i := range k {
+        maxSum += nums[i]
+    }
+    currSum := maxSum
+    for i := k; i < len(nums); i++ {
+        currSum = currSum + nums[i] - nums[i - k]
+        if maxSum < currSum {
+            maxSum = currSum
+        }
+    }
+
+    maxAvg = float64(maxSum) / float64(k)
+
+    return maxAvg
+}
