91a0b6cfe089ad866742984f579673fcf1b647ba
falearn <127325061+falearn228@users.noreply.github.com>
2025-08-07 02:50:11 +0700

    Time: 6 ms (56.00%) | Memory: 6.6 MB (99.11%) - LeetSync

---
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.go
@@ -0,0 +1,30 @@
+func maxVowels(s string, k int) int {
+    currVowel := 0
+
+    vowels := []byte{ 'a', 'e', 'i', 'o', 'u'}
+
+    for i := range k {
+        if slices.Contains(vowels, s[i]) {
+            currVowel++
+        }
+    }
+
+    maxVowel := currVowel
+    for i := k; i < len(s); i++ {
+        if slices.Contains(vowels, s[i]) {
+            currVowel++
+        }
+        if slices.Contains(vowels, s[i-k]) {
+            currVowel--
+        }
+        maxVowel = max(currVowel, maxVowel)
+    } 
+    return maxVowel
+}
+
+func max(a, b int) int {
+    if a > b {
+        return a
+    }
+    return b
+}
