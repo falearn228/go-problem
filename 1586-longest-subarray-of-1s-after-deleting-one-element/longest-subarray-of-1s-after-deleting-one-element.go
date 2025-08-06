f387f2282d74d2a5ee230ca81996d8f2ca3ad81b
falearn <127325061+falearn228@users.noreply.github.com>
2025-08-07 04:46:04 +0700

    Time: 0 ms (100.00%) | Memory: 8.9 MB (98.83%) - LeetSync

---
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.go
@@ -0,0 +1,23 @@
+func longestSubarray(nums []int) int {
+    k := 1
+    zeroNum := 0
+    left := 0 
+    maxLeng := 0
+    for right := 0; right < len(nums); right++ {
+        if nums[right] == 0 {
+            zeroNum++
+        }
+        for zeroNum > k {
+            if nums[left] == 0 {
+                zeroNum--
+            }
+            left++
+        }
+
+        currLeng := right - left
+        if currLeng > maxLeng {
+            maxLeng = currLeng
+        }
+    }
+    return maxLeng
+}
