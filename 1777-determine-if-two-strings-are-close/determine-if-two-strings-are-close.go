81bc1064262f280ba8433ed0e4b30d0f0e1a06be
falearn <127325061+falearn228@users.noreply.github.com>
2025-08-07 23:27:17 +0700

    Time: 37 ms (56.04%) | Memory: 8.6 MB (94.43%) - LeetSync

---
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.go
@@ -0,0 +1,46 @@
+func closeStrings(word1 string, word2 string) bool {
+    if len(word1) != len(word2) {
+        return false
+    }
+    // freq1 == freq2
+    freqMap1 := make(map[rune]int)
+    freqMap2 := make(map[rune]int) 
+    for _, v := range word1 {
+        freqMap1[v]++
+    }
+    for _, v := range word2 {
+        freqMap2[v]++
+        if _, ok := freqMap1[v]; !ok {
+            return false
+        }
+    }
+    if len(freqMap1) != len(freqMap2) {
+        return false
+    }
+    for char := range freqMap1 {
+        if _, ok := freqMap2[char]; !ok {
+            return false
+        }
+    }
+
+    freq1 := make([]int, len(freqMap1))
+    ind := 0
+    for _, v := range freqMap1 {
+        freq1[ind] = v
+        ind++
+    }
+    freq2 := make([]int, len(freqMap2))
+    ind = 0
+    for _, v := range freqMap2 {
+        freq2[ind] = v
+        ind++
+    }
+
+    slices.Sort(freq1)
+    slices.Sort(freq2)
+
+    if !slices.Equal(freq1, freq2) {
+        return false
+    }
+    return true
+}
