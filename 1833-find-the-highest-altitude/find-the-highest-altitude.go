90c131af8a3a1df8a6479365406fb365e4d8ebdf
falearn <127325061+falearn228@users.noreply.github.com>
2025-08-07 17:46:11 +0700

    Time: 0 ms (100.00%) | Memory: 4.1 MB (75.21%) - LeetSync

--- 1833-find-the-highest-altitude/find-the-highest-altitude.go
+++ 1833-find-the-highest-altitude/find-the-highest-altitude.go
@@ -1,17 +1,10 @@
 func largestAltitude(gain []int) int {
-    prefSum := make([]int, len(gain)+1)
-    maxAltitude := 0
-    for i := 1; i < len(prefSum); i++ {
-        prefSum[i] = prefSum[i-1] + gain[i-1]
-        maxAltitude = max(prefSum[i], maxAltitude)
+    var currAlt, maxAlt int
+    for _, v := range gain {
+        currAlt += v
+        if currAlt > maxAlt {
+            maxAlt = currAlt
+        }
     }
-
-    return maxAltitude
-}
-
-func max(a, b int) int {
-    if a > b {
-        return a
-    }
-    return b
+    return maxAlt 
 }
