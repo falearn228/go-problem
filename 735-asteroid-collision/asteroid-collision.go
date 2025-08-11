31360921afeccfe563a53c01adea7b91f08eaeca
falearn <127325061+falearn228@users.noreply.github.com>
2025-08-12 00:47:15 +0700

    Time: 0 ms (100.00%) | Memory: 6.7 MB (37.59%) - LeetSync

---
+++ 735-asteroid-collision/asteroid-collision.go
@@ -0,0 +1,23 @@
+func asteroidCollision(asteroids []int) []int {
+	stack := make([]int, 0, len(asteroids))
+
+	for i := 0; i < len(asteroids); i++ {
+		alive := true
+
+		for alive && asteroids[i] < 0 && len(stack) > 0 && stack[len(stack)-1] > 0 {
+			top := stack[len(stack)-1]
+			if top < -asteroids[i] {
+				stack = stack[:len(stack)-1]
+				continue
+			} else if top+asteroids[i] == 0 {
+				stack = stack[:len(stack)-1]
+			}
+			alive = false
+		}
+		if alive {
+			stack = append(stack, asteroids[i])
+		}
+	}
+
+	return stack
+}
