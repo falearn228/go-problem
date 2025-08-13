1c049ff0c2fd1520693a566a6e060b0227468ee2
falearn <127325061+falearn228@users.noreply.github.com>
2025-08-14 02:31:25 +0700

    Time: 0 ms (100.00%) | Memory: 4.4 MB (95.76%) - LeetSync

---
+++ 206-reverse-linked-list/reverse-linked-list.go
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reverseList(head *ListNode) *ListNode {
+    if head == nil || head.Next == nil {
+        return head
+    }
+    
+    var prev *ListNode
+    curr := head
+    for curr != nil {
+        nextPtr := curr.Next
+        curr.Next = prev
+        prev = curr
+        curr = nextPtr
+    }
+    return prev
+}
