e93a90e0b84bc399974e2218adb0855ad71a1d2a
falearn <127325061+falearn228@users.noreply.github.com>
2025-08-14 16:20:52 +0700

    Time: 111 ms (5.26%) | Memory: 12.9 MB (61.26%) - LeetSync

---
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.go
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func goodNodes(root *TreeNode) int {
+    count := 0
+
+    var dfs func(maxNode int, node *TreeNode)
+    dfs = func(maxNode int, node *TreeNode) {
+        if node == nil {
+            return 
+        }
+        if node.Val >= maxNode {
+            count++
+            maxNode = node.Val
+        }
+        dfs(maxNode, node.Left)
+        dfs(maxNode, node.Right)
+
+    }
+    dfs(root.Val, root) 
+
+    return count
+}
+
