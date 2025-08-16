968d8ad1e4db50a6038f2aa8bf8e849e95a8942d
falearn <127325061+falearn228@users.noreply.github.com>
2025-08-16 20:50:52 +0700

    Time: 14 ms (29.74%) | Memory: 6.3 MB (74.84%) - LeetSync

---
+++ 437-path-sum-iii/path-sum-iii.go
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+// if sumNodes > targetSum -> switch root (after left, right)
+func pathSum(root *TreeNode, targetSum int) int {
+    count := 0
+    var dfs func(node *TreeNode, need int) 
+    dfs = func(node *TreeNode, need int) {
+        if node == nil {
+            return
+        }
+        newNeed := need - node.Val
+        if newNeed == 0 {
+            count++
+        }
+        
+        dfs(node.Left, newNeed)
+        dfs(node.Right, newNeed)
+    }
+   
+    var walkStarts func(node *TreeNode)
+    walkStarts = func(node *TreeNode) {
+        if node == nil {
+            return
+        }
+        dfs(node, targetSum)
+        walkStarts(node.Left)
+        walkStarts(node.Right)
+        return
+    }
+    walkStarts(root)
+    return count
+}
