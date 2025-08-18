8bae60c5518bf3811d0e26871d82a732a041a8b6
falearn <127325061+falearn228@users.noreply.github.com>
2025-08-18 15:18:36 +0700

    Time: 0 ms (100.00%) | Memory: 4.3 MB (37.96%) - LeetSync

---
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.go
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func rightSideView(root *TreeNode) []int {
+    res := make([]int, 0)
+    if root == nil {
+        return res
+    }
+    var bfs func(n *TreeNode)
+    bfs = func(n *TreeNode) {
+        queue := make([]*TreeNode, 1)
+        queue[0] = n
+
+        for len(queue) > 0 {
+            size := len(queue)
+            var last *TreeNode
+
+            for i := 0; i < size; i++ {
+                node := queue[0]
+                queue[0] = nil
+                queue = queue[1:]
+
+                if node == nil {
+                    continue
+                }
+
+                last = node
+                if node.Left != nil {
+                    queue = append(queue, node.Left)
+                }
+                if node.Right != nil {
+                    queue = append(queue, node.Right)
+                }
+            }
+
+            if last != nil {
+                res = append(res, last.Val)
+            }
+        }
+    }
+    bfs(root)
+    return res
+}
