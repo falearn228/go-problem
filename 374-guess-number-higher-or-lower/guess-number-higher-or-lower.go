c3fff21434ae88c0983ab63499ac434739fb18e7
falearn <127325061+falearn228@users.noreply.github.com>
2025-08-19 14:58:16 +0700

    Time: 0 ms (100.00%) | Memory: 3.9 MB (15.18%) - LeetSync

---
+++ 374-guess-number-higher-or-lower/guess-number-higher-or-lower.go
@@ -0,0 +1,26 @@
+/** 
+ * Forward declaration of guess API.
+ * @param  num   your guess
+ * @return 	     -1 if num is higher than the picked number
+ *			      1 if num is lower than the picked number
+ *               otherwise return 0
+ * func guess(num int) int;
+ */
+
+func guessNumber(n int) int {
+    left := 1
+    right := n
+    for left <= right {
+        mid := left + (right-left) / 2
+
+        val := guess(mid)
+        if val == -1 {
+            right = mid - 1
+        } else if val == 1 {
+            left = mid+1
+        } else if val == 0 {
+            return mid
+        }
+    }
+    return left
+}
