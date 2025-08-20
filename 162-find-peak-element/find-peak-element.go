83a953f66813fe86af2b86e7ab9ebd1335c1d951
falearn <127325061+falearn228@users.noreply.github.com>
2025-08-20 17:34:18 +0700

    Time: 0 ms (100.00%) | Memory: 4.6 MB (64.04%) - LeetSync

--- 162-find-peak-element/find-peak-element.go
+++ 162-find-peak-element/find-peak-element.go
@@ -1,34 +1,12 @@
 func findPeakElement(nums []int) int {
-    n := len(nums)
-    left := 0
-    right := n - 1
-
-    for left <= right {
-        mid := left + (right - left) / 2
-
-        leftVal := math.MinInt
-        if mid-1 >= 0 {
-            leftVal = nums[mid-1]
-        }
-        rightVal := math.MinInt
-        if mid+1 < n {
-            rightVal = nums[mid+1]
-        }
-
-        currVal := nums[mid]
-        if currVal > leftVal && currVal > rightVal {
-            return mid
-        }
-
-        if currVal < rightVal {
-            left = mid+1
-        } else if currVal > rightVal  {
-            right = mid-1
-        } else if currVal < leftVal  {
-            left = mid+1
-        } else if currVal < leftVal  {
-            right = mid-1
+    l, r := 0, len(nums)-1
+    for l < r {
+        mid := l + (r-l)/2
+        if nums[mid] > nums[mid+1] {
+            r = mid
+        } else {
+            l = mid + 1
         }
     }
-    return left
+    return l
 }
