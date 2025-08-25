eb45fe7ce6fedb8e0e4bb1a814941d9c5c79fa11
falearn <127325061+falearn228@users.noreply.github.com>
2025-08-25 16:49:38 +0700

    Time: 43 ms (37.20%) | Memory: 19 MB (34.50%) - LeetSync

---
+++ 435-non-overlapping-intervals/non-overlapping-intervals.go
@@ -0,0 +1,21 @@
+func eraseOverlapIntervals(intervals [][]int) int {
+    if len(intervals) == 1 {
+        return 0
+    }
+
+    sort.Slice(intervals, func(i, j int) bool {
+        return intervals[i][1] < intervals[j][1]
+    })
+
+    lastEnd := intervals[0][1]
+    ans := 0
+    for i := 1; i < len(intervals); i++ {
+        start := intervals[i][0]
+        if start >= lastEnd {
+            lastEnd = intervals[i][1]
+        } else {
+            ans++
+        }
+    }
+    return ans
+}
