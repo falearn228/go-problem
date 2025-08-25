9ff138a9c6f5b99def5591d9724435d37b3c0ce2
falearn <127325061+falearn228@users.noreply.github.com>
2025-08-25 18:49:21 +0700

    Time: 49 ms (69.17%) | Memory: 17.1 MB (60.00%) - LeetSync

---
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.go
@@ -0,0 +1,22 @@
+func findMinArrowShots(points [][]int) int {
+    if len(points) == 1 {
+        return 1
+    }
+
+    sort.Slice(points, func(i, j int) bool {
+        return points[i][1] < points[j][1]
+    })
+
+    lastEnd := points[0][1]
+    answer := 1
+    for i := 1; i < len(points); i++ {
+        start := points[i][0]
+        if lastEnd >= start {
+            continue
+        } else {
+            lastEnd = points[i][1]
+            answer++
+        }
+    }
+    return answer
+}
