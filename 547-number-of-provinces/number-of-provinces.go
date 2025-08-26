a76b82d9b815bf834ee0b84be2c27f7f96ad1b09
falearn <127325061+falearn228@users.noreply.github.com>
2025-08-26 11:12:00 +0700

    Time: 0 ms (100.00%) | Memory: 9 MB (6.30%) - LeetSync

---
+++ 547-number-of-provinces/number-of-provinces.go
@@ -0,0 +1,29 @@
+func findCircleNum(isConnected [][]int) int {
+    visited := make([]bool , len(isConnected))
+    
+    provincies := 0
+
+    for i := 0; i < len(isConnected); i++ {
+        if !visited[i] {
+            provincies++
+            stack := []int{i}
+            for len(stack) > 0 {
+            room := stack[len(stack)-1]
+            stack = stack[:len(stack)-1]
+            if visited[room] {
+                continue
+            }
+            visited[room] = true
+
+            tops := isConnected[room]
+                for province, conn := range tops {
+                    if conn == 1 && !visited[province] {
+                        stack = append(stack, province)
+                    }
+                }
+            }
+        }
+    }
+
+    return provincies
+}
