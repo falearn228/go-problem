d08311c21520762e99f61d2eb70f2d9bc4235854
falearn <127325061+falearn228@users.noreply.github.com>
2025-08-26 10:55:06 +0700

    Time: 0 ms (100.00%) | Memory: 5.6 MB (43.64%) - LeetSync

---
+++ 871-keys-and-rooms/keys-and-rooms.go
@@ -0,0 +1,23 @@
+func canVisitAllRooms(rooms [][]int) bool { 
+    visited := make(map[int]bool)
+    stack := make([]int, 1)
+    visitedCount := 0
+    for len(stack) > 0 {
+        room := stack[len(stack)-1]
+        stack = stack[:len(stack)-1]
+        if visited[room] {
+            continue
+        }
+        visited[room] = true
+        visitedCount++
+
+        tops := rooms[room]
+        for _, child := range tops {
+            if !visited[child] {
+                stack = append(stack, child)
+            }
+        }
+    }
+    
+    return visitedCount == len(rooms)
+}
