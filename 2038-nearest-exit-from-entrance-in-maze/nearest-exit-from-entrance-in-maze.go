0abc6da6c2c37380bfe05ff2784460ed4fcb5977
falearn <127325061+falearn228@users.noreply.github.com>
2025-08-28 12:49:42 +0700

    Time: 4 ms (86.17%) | Memory: 8.8 MB (86.70%) - LeetSync

---
+++ 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.go
@@ -0,0 +1,49 @@
+func nearestExit(maze [][]byte, entrance []int) int {
+    queue := make([][2]int, 1)
+    queue[0][0], queue[0][1] = entrance[0], entrance[1]
+
+    maze[entrance[0]][entrance[1]] = '+'
+
+    steps := 0
+    moves := [4][2]int{
+        [2]int{1, 0},
+        [2]int{0, 1},
+        [2]int{-1, 0},
+        [2]int{0, -1},
+    }
+
+    endRow := len(maze)
+    endCol := len(maze[0])
+
+    for len(queue) > 0 {
+        steps++
+
+        lenQ := len(queue)
+        for i := 0; i < lenQ; i++ {
+            pos := queue[0]
+            queue = queue[1:]
+
+            for _, move := range moves {
+                row := pos[0] + move[0]
+                col := pos[1] + move[1]
+
+                if row < 0 || row >= endRow || col < 0 || col >= endCol {
+                    continue
+                }
+
+                if maze[row][col] == '+' {
+                    continue
+                }
+
+                if row == 0 || row == endRow-1 || col == 0 || col == endCol-1 {
+                    return steps
+                }
+
+                maze[row][col] = '+'
+                queue = append(queue, [2]int{row, col})
+            } 
+        }
+    }
+    
+    return -1
+}
