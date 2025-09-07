91b8908dd7127f32c72793330ddd022f9e52a7e3
falearn <127325061+falearn228@users.noreply.github.com>
2025-09-08 01:11:25 +0700

    Time: 0 ms (100.00%) | Memory: 4.1 MB (19.64%) - LeetSync

--- 27-remove-element/remove-element.go
+++ 27-remove-element/remove-element.go
@@ -1,25 +1,24 @@
 func removeElement(nums []int, val int) int {
-    if len(nums) == 0 {
+    n := len(nums)
+    if n == 0 {
         return 0
     }
 
-    var k int
-    tempNums := make([]int, len(nums))
-    
-    i1, i2 := 0, 0
-    for i1 < len(nums) {
-        if i2 >= 0 && nums[i1] != val {
-            tempNums[i2] = nums[i1]
-            i2++
+
+    curr1, curr2 := 0, n-1
+
+    for curr1 <= curr2 {
+        if nums[curr1] == val && nums[curr2] != val {
+            nums[curr1] = nums[curr2]
+            curr2--
+            curr1++
+        } else if nums[curr1] == val && nums[curr2] == val {
+            curr2--
+        } else {
+            curr1++
         }
-        i1++
-    }
-
-    k = i2
-    for i := 0; i <= i2 && i2 < len(nums); i++ {
-        nums[i] = tempNums[i]
     }
 
 
-    return k
+    return curr1
 }
