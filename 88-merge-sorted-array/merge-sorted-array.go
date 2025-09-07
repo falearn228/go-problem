b2c368a96ea6f79d61d1498589d8c06db270f6aa
falearn <127325061+falearn228@users.noreply.github.com>
2025-09-08 00:40:12 +0700

    Time: 0 ms (100.00%) | Memory: 4.2 MB (36.82%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.go
@@ -0,0 +1,18 @@
+func merge(nums1 []int, m int, nums2 []int, n int)  {
+    if n == 0 {
+        return
+    }
+    i1, i2, i3 := m - 1, n - 1, m + n - 1
+    
+    for i2 >= 0 {
+        if i1 >= 0 && nums1[i1] > nums2[i2] {
+            nums1[i3] = nums1[i1]
+            i1--
+        } else {
+            nums1[i3] = nums2[i2]
+            i2--
+        }
+        i3--
+    } 
+
+}
