29ea5eac39c09fb5987d6737b9090e11b5c8cab0
falearn <127325061+falearn228@users.noreply.github.com>
2025-09-08 10:36:12 +0700

    Time: 6 ms (77.34%) | Memory: 7.4 MB (34.59%) - LeetSync

---
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.go
@@ -0,0 +1,16 @@
+func removeDuplicates(nums []int) int {
+    n := len(nums)
+    if n == 1 {
+        return 1
+    }
+
+    insPos := 2
+
+    for i := 2; i < n; i++ {
+        if nums[i] != nums[insPos-2] {
+            nums[insPos] = nums[i]
+            insPos++
+        }
+    }
+    return insPos
+}
