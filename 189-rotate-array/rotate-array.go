5d1fce4c41c9f7577d33f05b3aa9d993c0a8a6b5
falearn <127325061+falearn228@users.noreply.github.com>
2025-09-08 11:53:40 +0700

    Time: 0 ms (100.00%) | Memory: 10 MB (27.13%) - LeetSync

--- 189-rotate-array/rotate-array.go
+++ 189-rotate-array/rotate-array.go
@@ -4,18 +4,12 @@ func rotate(nums []int, k int)  {
         return
     }
 
-    k %= n
+    arr := make([]int, n)
 
-    reverse(nums, 0, n-1)
-    reverse(nums, 0, k-1)
-    reverse(nums, k, n-1)
-}
-
-func reverse(nums []int, start, end int) {
-    for start < end+1 {
-        fmt.Println(start, end)
-        nums[start], nums[end] = nums[end], nums[start]
-        start++
-        end--
+    for i := range arr {
+        newpos := (k + i) % n
+        arr[newpos] = nums[i]
     }
+    
+    copy(nums, arr)
 }
