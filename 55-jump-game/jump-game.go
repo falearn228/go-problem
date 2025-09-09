5e6491b6bdf9724446dd31fafdb89f4ad34f4304
falearn <127325061+falearn228@users.noreply.github.com>
2025-09-09 15:57:12 +0700

    Time: 0 ms (100.00%) | Memory: 10.1 MB (15.95%) - LeetSync

--- 55-jump-game/jump-game.go
+++ 55-jump-game/jump-game.go
@@ -4,15 +4,17 @@ func canJump(nums []int) bool {
         return true
     }
 
-    var currjump int
-    for ind := 0; ind <= currjump; ind++ {
-        for start := ind; start < nums[ind] + ind; start++ {
-            currjump = max(nums[start] + start, currjump)
-            if currjump >= n-1 {
-                return true
-            }
+    var maxReach int
+    for i, jump := range nums {
+        if maxReach >= n-1  {
+            return true
         }
+        if i == n-1 || i > maxReach {
+            return false
+        }
+        maxReach = max(maxReach, i + jump)
     }
 
-    return false
+
+    return true
 }
