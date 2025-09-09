6eeddf5a3c93d0696e8f7e5f153ab38e5e4f88cd
falearn <127325061+falearn228@users.noreply.github.com>
2025-09-09 16:11:39 +0700

    Time: 0 ms (100.00%) | Memory: 7.7 MB (85.27%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.go
@@ -0,0 +1,26 @@
+func jump(nums []int) int {
+    len := len(nums)
+    if len == 1 {
+        return 0
+    }
+
+    currEnd := 0
+    count := 0
+    farthest := 0
+
+    for i := 0 ; i < len-1; i++ {
+        if i+nums[i] > farthest {
+            farthest = i + nums[i]
+        }
+
+        if i == currEnd {
+            count++
+            currEnd = farthest
+
+            if currEnd >= len-1 {
+                break
+            }
+        }
+    }
+    return count
+}
