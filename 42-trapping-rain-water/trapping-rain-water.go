5cf3b9e0d3709b1e4180982a4d0324bc5d3caee5
falearn <127325061+falearn228@users.noreply.github.com>
2025-09-12 12:32:34 +0700

    Time: 1011 ms (5.00%) | Memory: 8.5 MB (7.24%) - LeetSync

---
+++ 42-trapping-rain-water/trapping-rain-water.go
@@ -0,0 +1,28 @@
+func trap(height []int) int {
+    maxLefts := make([]int, len(height))
+    maxRights := make([]int, len(height))
+
+    for i := range height {
+        for j := 0; j < i; j++ {
+            maxLefts[i] = max(maxLefts[i], height[j]) 
+        }
+    }
+
+    for i := range height {
+        for j := len(height)-1; j > i; j-- {
+            maxRights[i] = max(maxRights[i], height[j]) 
+        }
+    }
+
+    var water int
+    for i := 0; i < len(height); i++ {
+        waterHeight := min(maxRights[i], maxLefts[i])
+        diff := waterHeight - height[i]
+        if diff > 0 {
+            water += diff
+        }
+        fmt.Println(waterHeight, waterHeight - height[i])
+    }
+
+    return water
+}
