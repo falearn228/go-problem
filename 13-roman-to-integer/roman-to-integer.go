4b5e20bd60efdcfca52cd06e9202f8e9f46895c2
falearn <127325061+falearn228@users.noreply.github.com>
2025-09-15 00:30:17 +0700

    Time: 0 ms (100.00%) | Memory: 4.8 MB (18.77%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.go
@@ -0,0 +1,20 @@
+func romanToInt(s string) int {
+	input := []byte{'I', 'V', 'X', 'L', 'C', 'D', 'M'}
+	output := []int{1, 5, 10, 50, 100, 500, 1000}
+
+	ans := 0
+	for i := range s {
+		rimlan := slices.Index(input, s[i])
+        num := output[rimlan]
+        if i+1 < len(s) && ((s[i] == 'I') && (s[i+1] == 'V' || s[i+1] == 'X')) {
+            num = -output[rimlan]
+        } else if i+1 < len(s) && ((s[i] == 'X') && (s[i+1] == 'L' || s[i+1] == 'C')) {
+            num = -output[rimlan]
+        } else if i+1 < len(s) && ((s[i] == 'C') && (s[i+1] == 'D' || s[i+1] == 'M')) {
+            num = -output[rimlan]
+        }
+		ans += num
+	}
+
+	return ans
+}
