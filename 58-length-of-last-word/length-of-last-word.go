eef97bb3bc6ea5d1994046af9fa7db229a8a18a8
falearn <127325061+falearn228@users.noreply.github.com>
2025-09-15 02:14:42 +0700

    Time: 0 ms (100.00%) | Memory: 4 MB (83.90%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.go
+++ 58-length-of-last-word/length-of-last-word.go
@@ -1,4 +1,20 @@
 func lengthOfLastWord(s string) int {
-    ans := strings.Fields(s)
-    return len(ans[len(ans)-1])
+    ans := 0
+    isFirstSpace := true
+
+    for i := len(s)-1; i >= 0; i-- {
+        if isFirstSpace && s[i] == ' ' {
+            continue
+        } else {
+            isFirstSpace = false
+        }
+
+        if s[i] != ' ' {
+            ans += 1
+        } else {
+            break
+        }
+    }
+
+    return ans
 }
