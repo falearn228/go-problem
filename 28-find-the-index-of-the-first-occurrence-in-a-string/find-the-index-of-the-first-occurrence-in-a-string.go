56ab7dae3a1c6e933a583e0e3d0c6dd53d8f1719
falearn <127325061+falearn228@users.noreply.github.com>
2025-09-15 15:15:54 +0700

    Time: 0 ms (100.00%) | Memory: 4 MB (8.40%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.go
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.go
@@ -5,17 +5,54 @@ func strStr(haystack string, needle string) int {
         return -1
     }
 
-    for i := 0; i <= hlen-nlen; i++ {
-        j := 0
-        for j < nlen && haystack[i+j] == needle[j] {
+    // O(n*m) - интуитивное решение
+    // for i := 0; i <= hlen-nlen; i++ {
+    //     j := 0
+    //     for j < nlen && haystack[i+j] == needle[j] {
+    //         j++
+    //     }
+
+    //     if j == nlen {
+    //         return i
+    //     }
+    // }
+    // return -1
+
+    lps := make([]int, nlen)
+    length := 0
+    i := 1
+    for i < nlen {
+        if needle[i] == needle[length] {
+            length++
+            lps[i] = length
+            i++
+        } else {
+            if length != 0 {
+                length = lps[length-1]
+            } else {
+                lps[i] = 0
+                i++
+            }
+        }
+    }
+
+    i = 0
+    j := 0
+    for i < hlen {
+        if needle[j] == haystack[i] {
+            i++
             j++
         }
 
         if j == nlen {
-            return i
+            return i-j
+        } else if i < hlen && needle[j] != haystack[i] {
+            if j != 0 {
+                j = lps[j-1]
+            } else {
+                i++
+            }
         }
     }
-
-
     return -1
 }
