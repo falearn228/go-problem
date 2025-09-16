040fc63066e6d700d12f7054546ce03e2be6ac0f
falearn <127325061+falearn228@users.noreply.github.com>
2025-09-16 15:58:13 +0700

    Time: 4 ms (27.54%) | Memory: 4.8 MB (53.02%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.go
@@ -0,0 +1,31 @@
+func isPalindrome(s string) bool {
+    s = isLetter(s)
+    s = strings.ToLower(s)
+
+    if len(s) == 0 {
+        return true
+    }
+    left, right := 0, len(s)-1
+
+    for left < right {
+        if s[left] != s[right] {
+            return false
+        }
+
+        left++
+        right--
+    }
+
+    return true
+}
+
+func isLetter(s string) string {
+	var b strings.Builder
+	b.Grow(len(s))
+	for _, v := range s {
+		if unicode.IsLetter(v) || unicode.IsNumber(v) {
+			b.WriteRune(v)
+		}
+	}
+	return b.String()
+}
