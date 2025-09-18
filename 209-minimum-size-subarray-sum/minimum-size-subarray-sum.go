f53fe814ab7d0bf35db00b8c213020d276e0d351
falearn <127325061+falearn228@users.noreply.github.com>
2025-09-18 13:30:09 +0700

    Time: 0 ms (100.00%) | Memory: 9.5 MB (37.22%) - LeetSync

---
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.go
@@ -0,0 +1,17 @@
+func minSubArrayLen(target int, nums []int) int {
+    left := 0
+    sum := 0
+    ans := 1_000_000_000
+    for right := 0; right < len(nums); right++ {
+        sum += nums[right]
+        for sum >= target {
+            ans = min(ans, right - left+1)
+            sum -= nums[left]
+            left++
+        }
+    }
+    if ans == 1_000_000_000 {
+        return 0
+    }
+    return ans
+}
