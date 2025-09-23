788963d30eaf754a3bc771509a54914c8c68ecaa
falearn <127325061+falearn228@users.noreply.github.com>
2025-09-23 16:57:56 +0700

    Time: 32 ms (29.22%) | Memory: 5.3 MB (27.94%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.go
@@ -0,0 +1,53 @@
+func minWindow(s string, t string) string {
+    subLen := len(t)
+    strLen := len(s)
+
+    if subLen > strLen {
+        return ""
+    }
+
+    minLen := ""
+    byte_c := make(map[byte]int)
+    for i := range t {
+        byte_c[t[i]]++
+    }
+
+    left := 0 
+    curr_byte_c := make(map[byte]int)
+    curr_len := 0
+    need_len := len(byte_c) // len keys -> unique symbs
+
+    for right := left; right < strLen; right++ {
+        char := s[right]
+        if _, ok := byte_c[char]; !ok {
+            continue
+        }
+        
+        curr_byte_c[char]++
+
+        if curr_byte_c[char] == byte_c[char] {
+            curr_len++
+        }
+
+        for curr_len == need_len {
+            if minLen == "" || (right-left+1) < len(minLen) {
+                minLen = s[left:right+1]
+            }
+
+            left_c := s[left]
+
+            if cnt, ok := byte_c[left_c]; ok {
+                if curr_byte_c[left_c] == cnt {
+                    curr_len--
+                }
+                curr_byte_c[left_c]--
+            }
+
+            left++
+        }
+
+    }
+    
+
+    return minLen
+}
