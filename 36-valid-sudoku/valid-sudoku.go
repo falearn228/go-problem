b6ae8507e6156d97948701a9164a373b8b72c70a
falearn <127325061+falearn228@users.noreply.github.com>
2025-09-24 15:08:48 +0700

    Time: 0 ms (100.00%) | Memory: 5.7 MB (15.77%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.go
@@ -0,0 +1,39 @@
+func isValidSudoku(board [][]byte) bool {
+    rows := make([]map[byte]bool, 9)
+    cols :=  make([]map[byte]bool, 9)
+    squares := make([]map[byte]bool, 9)
+    
+    for i := 0; i < len(rows); i++ {
+        rows[i] = make(map[byte]bool)
+        cols[i] = make(map[byte]bool)
+        squares[i] = make(map[byte]bool)
+    }
+
+    for i := range board {
+        for j := range board[0] {
+            num := board[i][j]
+            if num == '.' || num == ',' {
+                continue
+            } 
+
+            if rows[i][num] == true {
+                return false
+            } 
+            rows[i][num] = true
+
+            if cols[j][num] == true {
+                return false
+            } 
+            cols[j][num] = true
+
+            box := (i/3)*3 + j/3 
+            if squares[box][num] == true {
+                return false
+            } 
+            squares[box][num] = true
+        }
+
+    }
+
+    return true
+}
