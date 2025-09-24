dc6d10cd578f783653e3bbb2f869fd0ed646cb6f
falearn <127325061+falearn228@users.noreply.github.com>
2025-09-24 16:43:56 +0700

    Time: 0 ms (100.00%) | Memory: 3.9 MB (49.56%) - LeetSync

---
+++ 54-spiral-matrix/spiral-matrix.go
@@ -0,0 +1,44 @@
+func spiralOrder(matrix [][]int) []int {
+    if len(matrix) == 0 {
+        return []int{}
+    }
+
+    rows := len(matrix)
+    cols := len(matrix[0])
+    ans := make([]int, 0, rows*cols)
+
+    top, bottom := 0, rows-1
+    left, right := 0, cols-1
+
+    for top <= bottom && left <= right {
+        for j := left; j <= right; j++ {
+            ans = append(ans, matrix[top][j])
+        }
+        top++
+
+        if top > bottom {
+            break
+        }
+
+        for i := top; i <= bottom; i++ {
+            ans = append(ans, matrix[i][right])
+        }
+        right--
+
+        if right < left {
+            break
+        }
+
+        for j := right; j >= left; j-- {
+            ans = append(ans, matrix[bottom][j])
+        }
+        bottom--
+
+        for i := bottom; i >= top; i-- {
+            ans = append(ans, matrix[i][left])
+        }
+        left++
+    }
+
+    return ans
+}
