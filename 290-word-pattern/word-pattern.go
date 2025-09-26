5aa9a9f73cdfd5a220bcbe11da66eef8d07f73b4
falearn <127325061+falearn228@users.noreply.github.com>
2025-09-26 10:30:27 +0700

    Time: 0 ms (100.00%) | Memory: 3.9 MB (67.82%) - LeetSync

---
+++ 290-word-pattern/word-pattern.go
@@ -0,0 +1,28 @@
+func wordPattern(pattern string, s string) bool {
+    govno := make(map[string]byte)
+    mocha := make(map[byte]string)
+
+    fields := strings.Fields(s)
+    if len(fields) != len(pattern) {
+        return false
+    }
+    for i := range fields {
+        char1, ok1 := govno[fields[i]]
+        char2, ok2 := mocha[pattern[i]]
+        if ok1 {
+            if char1 != pattern[i] {
+                return false
+            }
+        }
+
+        if ok2 {
+            if char2 != fields[i] {
+                return false
+            }
+        }
+        govno[fields[i]] = pattern[i]
+        mocha[pattern[i]] = fields[i]
+    }
+
+    return true
+}
