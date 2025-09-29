90e4f689d71604fbb3374e30c4bddf22b05400be
falearn <127325061+falearn228@users.noreply.github.com>
2025-09-29 15:35:39 +0700

    Time: 0 ms (100.00%) | Memory: 5.8 MB (68.40%) - LeetSync

--- 1-two-sum/two-sum.go
+++ 1-two-sum/two-sum.go
@@ -1,15 +1,12 @@
 func twoSum(nums []int, k int) []int {
     zalupa := make(map[int]int, len(nums))
 
-    for i, v := range nums {
-        zalupa[k-v] = i 
-    }
-
     for i := range nums {
         value := nums[i]
         if index, ok := zalupa[value]; ok && i != index {
             return []int{index, i}
-        } 
+        }
+        zalupa[k-value]=i
     }
 
     return []int{}
