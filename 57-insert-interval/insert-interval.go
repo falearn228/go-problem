2075cce66ff5605536807dc7b02b7275f911ad6b
falearn <127325061+falearn228@users.noreply.github.com>
2025-10-01 13:17:20 +0700

    Time: 0 ms (100.00%) | Memory: 5.9 MB (99.52%) - LeetSync

--- 57-insert-interval/insert-interval.go
+++ 57-insert-interval/insert-interval.go
@@ -1,52 +1,23 @@
 func insert(intervals [][]int, newInterval []int) [][]int {
-	if len(intervals) == 0 {
-		return [][]int{newInterval}
-	}
-	ans := make([][]int, 0, len(intervals))
+	ans := make([][]int, 0, len(intervals)+1)
 
-	left := 0
-	right := len(intervals) - 1
-	for left <= right {
-		mid := (left + right) / 2
-
-		if intervals[mid][0] > newInterval[0] {
-			right = mid - 1
-		} else {
-			left = mid + 1
-		}
-	}
-	for i := range left {
+	i := 0
+	for i < len(intervals) && intervals[i][1] < newInterval[0] {
 		ans = append(ans, intervals[i])
+		i++
 	}
-	// ans = append(ans, newInterval)
 
-	//вставка newInterval
-	var last []int
-	if len(ans) > 0 {
-		last = ans[len(ans)-1]
-		if newInterval[0] <= last[1] {
-			if newInterval[1] > last[1] {
-				ans[len(ans)-1][1] = newInterval[1]
-			}
-		} else {
-			ans = append(ans, newInterval)
-		}
-	} else {
-		ans = append(ans, newInterval)
+	// 1 6  5 10
+	for i < len(intervals) && intervals[i][0] <= newInterval[1] {
+		newInterval[0] = min(intervals[i][0], newInterval[0])
+		newInterval[1] = max(intervals[i][1], newInterval[1])
+		i++
 	}
-	
-	// merge
-	for i := left; i < len(intervals); i++ {
-		last = ans[len(ans)-1]
-		curr := intervals[i]
+	ans = append(ans, newInterval)
 
-		if curr[0] <= last[1] {
-			if curr[1] > last[1] {
-				ans[len(ans)-1][1] = curr[1]
-			}
-		} else {
-			ans = append(ans, curr)
-		}
+	for i < len(intervals) {
+		ans = append(ans, intervals[i])
+		i++
 	}
 
 	return ans
