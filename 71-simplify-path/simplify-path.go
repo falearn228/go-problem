56ad3203322810916d046fbb1f371747c23aeafc
falearn <127325061+falearn228@users.noreply.github.com>
2025-10-03 12:05:42 +0700

    Time: 0 ms (100.00%) | Memory: 5.4 MB (32.14%) - LeetSync

--- 71-simplify-path/simplify-path.go
+++ 71-simplify-path/simplify-path.go
@@ -1,31 +1,22 @@
 func simplifyPath(path string) string {
-	stack := []string{}
+	stack := make([]string, 0, len(path))
 	fields := strings.Split(path, "/")
 
-	for i := range fields {
-		if len(stack) > 0 && fields[i] == ".." {
-			stack = stack[:len(stack)-1]
-		} else if fields[i] == "" || fields[i] == "." || fields[i] == ".."  {
+	for _, v := range fields {
+		if v == "" {
 			continue
-		} else {
-			stack = append(stack, fields[i])
+		}
+
+		switch v {
+		case ".":
+		case "..":
+			if len(stack) > 0 {
+				stack = stack[:len(stack)-1]
+			}
+        default:
+		    stack = append(stack,v)
 		}
 	}
 
-    var preres string
-    if  len(stack) > 0 && stack[0] != "/" {
-        preres = "/" + strings.Join(stack, "/")
-    } else {
-	    preres = strings.Join(stack, "/")
-    }
-
-    if len(preres) == 0 {
-        return "/"
-    }
-
-	if len(preres) > 0 && preres[len(preres)-1] == '/' {
-		return preres[:len(preres)-1]
-	} 
-
-	return preres
+	return "/" + strings.Join(stack, "/")
 }
