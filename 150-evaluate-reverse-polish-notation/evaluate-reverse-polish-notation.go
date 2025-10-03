32e6a196457b9582db667eb5e481c03315e6a869
falearn <127325061+falearn228@users.noreply.github.com>
2025-10-03 16:24:20 +0700

    Time: 0 ms (100.00%) | Memory: 6.1 MB (55.77%) - LeetSync

---
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.go
@@ -0,0 +1,36 @@
+func evalRPN(tokens []string) int {
+	stack := make([]int, 0, len(tokens))
+
+	operators := map[string]struct{}{
+		"+": struct{}{},
+		"-": struct{}{},
+		"*": struct{}{},
+		"/": struct{}{},
+	}
+
+	for _, v := range tokens {
+		if _, ok := operators[v]; !ok {
+			intval, _ := strconv.Atoi(string(v))
+			stack = append(stack, intval)
+		} else {
+			var ans int
+			num1 := stack[len(stack)-1]
+			num2 := stack[len(stack)-2]
+			stack = stack[:len(stack)-2]
+
+			switch v {
+			case "+":
+				ans += num2 + num1
+			case "-":
+				ans += num2 - num1
+			case "*":
+				ans += num2 * num1
+			case "/":
+				ans += num2 / num1
+			}
+			stack = append(stack, ans)
+		}
+	}
+
+	return stack[0]
+}
