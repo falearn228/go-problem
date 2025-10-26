d5640454354e8f288a56bd099a45f958431bc052
falearn <127325061+falearn228@users.noreply.github.com>
2025-10-27 01:23:35 +0700

    Time: 4 ms (92.13%) | Memory: 7.9 MB (82.02%) - LeetSync

---
+++ 772-construct-quad-tree/construct-quad-tree.go
@@ -0,0 +1,49 @@
+/**
+ * Definition for a QuadTree node.
+ * type Node struct {
+ *     Val bool
+ *     IsLeaf bool
+ *     TopLeft *Node
+ *     TopRight *Node
+ *     BottomLeft *Node
+ *     BottomRight *Node
+ * }
+ */
+
+func construct(grid [][]int) *Node {
+    var dfs func(x, y, size int) *Node  
+
+    dfs = func(x, y, size int) *Node {
+        isEqual := true
+        firstVal := grid[x][y]
+l:
+        for i := x; i < x+size; i++ {
+            for j := y; j < y+size; j++ {
+                if grid[i][j] != firstVal {
+                    isEqual = false
+                    break l
+                }
+            }
+        }
+
+        if isEqual {
+            return &Node{Val: firstVal == 1, IsLeaf: true}
+        }
+
+        newSize := size / 2
+        node := &Node{
+            IsLeaf: false,
+            Val: true,
+        }
+
+        node.TopLeft  = dfs(x, y, newSize)
+        node.TopRight = dfs(x, y+newSize, newSize)
+        node.BottomLeft = dfs(x+newSize, y, newSize)
+        node.BottomRight = dfs(x+newSize, y+newSize, newSize)
+
+        return node
+    }
+
+    root := dfs(0, 0, len(grid))
+    return root
+}
