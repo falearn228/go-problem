7ca2d1cb65491467a37e82a80212081dc995ad3e
falearn <127325061+falearn228@users.noreply.github.com>
2025-11-01 01:06:43 +0700

    Time: 3 ms (52.23%) | Memory: 5.4 MB (35.04%) - LeetSync

---
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.go
@@ -0,0 +1,25 @@
+/**
+ * Definition for a Node.
+ * type Node struct {
+ *     Val int
+ *     Next *Node
+ *     Random *Node
+ * }
+ */
+
+func copyRandomList(head *Node) *Node {
+    nodeMap := make(map[*Node]*Node)
+    curr := head
+
+    for ; curr != nil; curr = curr.Next {
+        nodeMap[curr] = &Node{Val: curr.Val}
+    }
+
+    curr = head
+    for ;curr != nil; curr = curr.Next {
+        nodeMap[curr].Next = nodeMap[curr.Next]
+        nodeMap[curr].Random = nodeMap[curr.Random]
+    }
+    newDeepNode := nodeMap[head]
+    return newDeepNode
+}
