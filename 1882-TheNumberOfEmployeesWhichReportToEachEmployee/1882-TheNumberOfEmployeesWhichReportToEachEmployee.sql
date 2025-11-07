c1f466d126054a2d2ab88f1158884991af7529c7
falearn <127325061+falearn228@users.noreply.github.com>
2025-11-07 14:42:51 +0700

    Create 1882-TheNumberOfEmployeesWhichReportToEachEmployee

---
+++ 1882-TheNumberOfEmployeesWhichReportToEachEmployee/1882-TheNumberOfEmployeesWhichReportToEachEmployee.sql
@@ -0,0 +1,7 @@
+-- Last updated: 11/7/2025, 2:46:21 PM
+-- Write your PostgreSQL query statement below
+select e1.employee_id, e1.name, count(*) as reports_count , round(avg(e2.age)) as average_age 
+from Employees as e1
+join Employees as e2 on e1.employee_id = e2.reports_to
+group by e1.employee_id, e1.name
+order by e1.employee_id
