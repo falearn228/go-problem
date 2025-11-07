24c7068e3eff1c12d6dc75b9c34400d661b65273
falearn <127325061+falearn228@users.noreply.github.com>
2025-11-07 14:44:18 +0700

    Create 452-MinimumNumberOfArrowsToBurstBalloons

---
+++ 452-MinimumNumberOfArrowsToBurstBalloons/452-MinimumNumberOfArrowsToBurstBalloons.go
@@ -0,0 +1,21 @@
+// Last updated: 11/7/2025, 2:47:48 PM
+func findMinArrowShots(points [][]int) int {
+    if len(points) == 1 {
+        return 1
+    }
+
+    sort.Slice(points, func(i, j int) bool {
+        return points[i][1] < points[j][1]
+    })
+
+    lastEnd := points[0][1]
+    answer := 1
+    for i := 1; i < len(points); i++ {
+        start := points[i][0]
+        if lastEnd < start {
+            lastEnd = points[i][1]
+            answer++
+        }
+    }
+    return answer
+}
