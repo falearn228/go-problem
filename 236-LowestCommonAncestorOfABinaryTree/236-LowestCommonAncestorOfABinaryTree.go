3f83cd1376721936ea1e3790c7daf414f4f365f4
falearn <127325061+falearn228@users.noreply.github.com>
2025-11-07 14:44:41 +0700

    Create 236-LowestCommonAncestorOfABinaryTree

---
+++ 236-LowestCommonAncestorOfABinaryTree/236-LowestCommonAncestorOfABinaryTree.go
@@ -0,0 +1,34 @@
+// Last updated: 11/7/2025, 2:48:12 PM
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+    var dfs func(node *TreeNode) *TreeNode
+    dfs = func(node *TreeNode) *TreeNode {
+        if node == nil {
+            return nil
+        }
+
+        if node == p || node == q {
+            return node
+        }
+        lNode := dfs(node.Left)
+        rNode := dfs(node.Right)
+
+        if lNode != nil && rNode != nil {
+            return node
+        }
+        if rNode == nil  {
+            return lNode
+        }
+        return rNode
+
+    }
+    answer := dfs(root)
+    return answer
+}
