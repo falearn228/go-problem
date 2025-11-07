b07bb245329dca333d24d56b2384efe2f1d1560c
falearn <127325061+falearn228@users.noreply.github.com>
2025-11-07 14:45:20 +0700

    Create 134-GasStation

---
+++ 134-GasStation/134-GasStation.go
@@ -0,0 +1,22 @@
+// Last updated: 11/7/2025, 2:48:51 PM
+func canCompleteCircuit(gas []int, cost []int) int {
+    var totalTank, currTank, i, currStation int
+
+    n := len(gas)
+    for i < n {
+        diff := gas[i] - cost[i]
+        totalTank += diff
+        currTank += diff
+        if currTank < 0 { // ушли в минус по бензину, недоживем до следующей станции 
+            // обновляем станцию, станции ниже i не могут быть стартовыми
+            currStation = i + 1
+            currTank = 0
+        }
+        i++
+    } 
+
+    if totalTank < 0 {
+        return -1
+    }
+    return currStation
+}
