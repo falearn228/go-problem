c9d1abeca168e06d7bd1788132cf7ad49fdd13c7
falearn <127325061+falearn228@users.noreply.github.com>
2025-11-07 14:45:47 +0700

    Create 45-JumpGameIi

---
+++ 45-JumpGameIi/45-JumpGameIi.go
@@ -0,0 +1,28 @@
+// Last updated: 11/7/2025, 2:49:17 PM
+func jump(nums []int) int {
+    len := len(nums)
+    if len == 1 {
+        return 0
+    }
+
+    currEnd := 0
+    count := 0
+    farthest := 0
+
+    for i := 0 ; i < len-1; i++ {
+        if i+nums[i] > farthest {
+            farthest = i + nums[i]
+        }
+        
+
+        if i == currEnd  || farthest >= len-1{
+            count++
+            currEnd = farthest
+
+            if currEnd >= len-1 {
+                break
+            }
+        }
+    }
+    return count
+}
