42b18c7d627606dd8af21451789d8355c0e38359
falearn <127325061+falearn228@users.noreply.github.com>
2025-11-07 14:45:58 +0700

    Create 20-ValidParentheses

---
+++ 20-ValidParentheses/20-ValidParentheses.go
@@ -0,0 +1,26 @@
+// Last updated: 11/7/2025, 2:49:28 PM
+func isValid(s string) bool {
+	stack := make([]byte, 0, len(s))
+
+    pairs := map[byte]byte{
+        ')': '(',
+        '}': '{',
+        ']': '[',
+    }
+
+    for i := range s {
+        char := s[i]
+        
+        if open, isClosing := pairs[char]; isClosing {
+            if len(stack) == 0 || stack[len(stack)-1] != open {
+               return false
+            }
+            stack = stack[:len(stack)-1]
+        } else {
+            stack = append(stack, char)
+        }
+    }
+
+
+	return len(stack) == 0
+}
