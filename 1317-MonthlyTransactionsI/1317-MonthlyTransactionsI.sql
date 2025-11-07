233ae3be62a85bfccf0b80ea506dcbd68b34e274
falearn <127325061+falearn228@users.noreply.github.com>
2025-11-07 14:43:23 +0700

    Create 1317-MonthlyTransactionsI

---
+++ 1317-MonthlyTransactionsI/1317-MonthlyTransactionsI.sql
@@ -0,0 +1,8 @@
+-- Last updated: 11/7/2025, 2:46:54 PM
+
+select to_char(trans_date, 'YYYY-MM') as month, country,
+count(id) as trans_count, sum(case when state = 'approved' then 1 else 0 end) as approved_count,
+sum(amount) as trans_total_amount,
+sum(case when state = 'approved' then amount else 0 end) as approved_total_amount
+from Transactions
+group by month, country
