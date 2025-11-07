74c2297e6b2d2f5befc02744e996a68f0a5228f4
falearn <127325061+falearn228@users.noreply.github.com>
2025-11-07 14:45:45 +0700

    Create 48-RotateImage

---
+++ 48-RotateImage/48-RotateImage.go
@@ -0,0 +1,16 @@
+// Last updated: 11/7/2025, 2:49:15 PM
+func rotate(matrix [][]int)  {
+    n := len(matrix)
+    // step 1. a -> a^T
+    for i := 0; i < n; i++ {
+        for j := i ; j < n; j++ {
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        }
+    }
+    // step 2. reverse column
+    for row := 0; row < n; row++ {
+        for i, j := 0, n-1; i < j; i, j = i+1, j-1 {
+            matrix[row][i], matrix[row][j] = matrix[row][j], matrix[row][i]
+        }
+    }
+}
