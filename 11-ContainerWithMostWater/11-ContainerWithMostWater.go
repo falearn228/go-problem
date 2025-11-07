b2dbfaac7e33aab42e99328dda4c2e5b6094a94d
falearn <127325061+falearn228@users.noreply.github.com>
2025-11-07 14:46:07 +0700

    Create 11-ContainerWithMostWater

---
+++ 11-ContainerWithMostWater/11-ContainerWithMostWater.go
@@ -0,0 +1,31 @@
+// Last updated: 11/7/2025, 2:49:37 PM
+func maxArea(height []int) int {
+    // Y * X -> min(y1, y2) * (x2-x1) = 49
+
+    n := len(height)
+    if n == 2 {
+        return min(height[1], height[0])
+    }
+
+    i, j := 0, n-1
+
+    maxSquare := 0
+    for i < j {
+        square := min(height[i], height[j]) * (j-i)
+        maxSquare = max(maxSquare, square)
+        if height[i] < height[j] {
+            i++
+        } else {
+            j--
+        }
+    }
+
+    return maxSquare
+}
+
+func min(a, b int) int {
+    if a < b {
+        return a
+    }
+    return b
+}
