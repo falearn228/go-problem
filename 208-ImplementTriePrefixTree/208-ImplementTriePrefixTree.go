38f977153556364c091a8875e261224447252452
falearn <127325061+falearn228@users.noreply.github.com>
2025-11-07 14:44:49 +0700

    Create 208-ImplementTriePrefixTree

---
+++ 208-ImplementTriePrefixTree/208-ImplementTriePrefixTree.go
@@ -0,0 +1,52 @@
+// Last updated: 11/7/2025, 2:48:19 PM
+type Trie struct {
+	child [26]*Trie
+	isEnd bool
+}
+
+func Constructor() Trie {
+	return Trie{}
+}
+
+func (this *Trie) Insert(word string) {
+	current := this
+	for i := range word {
+		if current.child[word[i]-'a'] == nil {
+			current.child[word[i]-'a'] = &Trie{}
+		}
+		current = current.child[word[i]-'a']
+	}
+	current.isEnd = true
+}
+
+func (this *Trie) Search(word string) bool {
+	curr := this
+	for i := range word {
+		if curr.child[word[i]-'a'] == nil {
+			return false
+		}
+		curr = curr.child[word[i]-'a']
+	}
+	return curr.isEnd
+}
+
+func (this *Trie) StartsWith(prefix string) bool {
+	curr := this
+	for i := range prefix {
+		if curr.child[prefix[i]-'a'] == nil {
+			return false
+		}
+		curr = curr.child[prefix[i]-'a']
+	}
+	return true
+}
+
+
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * obj := Constructor();
+ * obj.Insert(word);
+ * param_2 := obj.Search(word);
+ * param_3 := obj.StartsWith(prefix);
+ */
