efb8a27488da9a83adb7ae1c8422e8524686aeb4
falearn <127325061+falearn228@users.noreply.github.com>
2025-11-07 14:46:06 +0700

    Create 14-LongestCommonPrefix

---
+++ 14-LongestCommonPrefix/14-LongestCommonPrefix.cpp
@@ -0,0 +1,12 @@
+// Last updated: 11/7/2025, 2:49:36 PM
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        if (strs.empty()) return "";
+        string prefix = strs[0];
+        for (string s : strs)
+            while (s.find(prefix) != 0)
+                prefix = prefix.substr(0, prefix.length() - 1);
+        return prefix;
+    }
+};
