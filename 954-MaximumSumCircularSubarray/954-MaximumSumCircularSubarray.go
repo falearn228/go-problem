b16d2db481633d3a4a5153b7bf5096b7ceef1e7e
falearn <127325061+falearn228@users.noreply.github.com>
2025-11-07 14:43:42 +0700

    Create 954-MaximumSumCircularSubarray

---
+++ 954-MaximumSumCircularSubarray/954-MaximumSumCircularSubarray.go
@@ -0,0 +1,28 @@
+// Last updated: 11/7/2025, 2:47:12 PM
+func maxSubarraySumCircular(nums []int) int {
+    minCurr := nums[0]
+    minGlobal := nums[0]
+    maxKad := nums[0]
+    maxKadCurr := nums[0]
+    sum := nums[0]
+    n := len(nums)
+    for i := 1; i < n; i++ {
+        num := nums[i]
+        maxKadCurr = max(maxKadCurr + num, num)
+        maxKad = max(maxKad, maxKadCurr) 
+        minCurr = min(minCurr + num, num)
+        minGlobal = min(minGlobal, minCurr) 
+        sum += num
+    }
+
+    if sum == minGlobal {
+        return maxKad
+    }
+
+    maxGlobal := sum - minGlobal
+
+
+    return max(maxGlobal, maxKad)
+}
+
+// 1 2 3 1 2 3
