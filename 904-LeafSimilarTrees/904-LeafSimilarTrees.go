2595fc4cd8bd5b6ec107af779ceabd149ca25141
falearn <127325061+falearn228@users.noreply.github.com>
2025-11-07 14:43:44 +0700

    Create 904-LeafSimilarTrees

---
+++ 904-LeafSimilarTrees/904-LeafSimilarTrees.go
@@ -0,0 +1,39 @@
+// Last updated: 11/7/2025, 2:47:15 PM
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func leafSimilar(root1 *TreeNode, root2 *TreeNode) bool {
+    leafs1 := collectLeaves(root1)
+    leafs2 := collectLeaves(root2)
+    fmt.Println(leafs1, leafs2)
+
+    return slices.Equal(leafs1, leafs2)
+}
+
+func collectLeaves(node *TreeNode) []int {
+    res := []int{}
+    stack := []*TreeNode{node}
+    var dfs func(n *TreeNode)
+    dfs = func(n *TreeNode) {
+        for len(stack) > 0 {
+            n := stack[len(stack)-1]
+            stack = stack[:len(stack)-1]
+            if n == nil {
+                continue
+            }
+            if n.Left == nil && n.Right == nil {
+                res = append(res, n.Val)
+                continue
+            }
+            stack = append(stack, n.Left)
+            stack = append(stack, n.Right)
+        }
+    }
+    dfs(node)
+    return res
+} 
