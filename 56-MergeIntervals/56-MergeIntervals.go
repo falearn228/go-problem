379da0cd756664dd2f42f5e34e1843d9db92eebb
falearn <127325061+falearn228@users.noreply.github.com>
2025-11-07 14:45:40 +0700

    Create 56-MergeIntervals

---
+++ 56-MergeIntervals/56-MergeIntervals.go
@@ -0,0 +1,25 @@
+// Last updated: 11/7/2025, 2:49:10 PM
+// arr1[end] >= arr2[start] 
+// 1,4   4,7
+func merge(intervals [][]int) [][]int {
+    slices.SortFunc(intervals, func(i, j []int) int {
+		return int(i[0] - j[0])
+	})
+
+    ans := make([][]int, 0, len(intervals))
+    ans = append(ans, intervals[0])
+    for i := 1; i < len(intervals); i++ {
+        last := ans[len(ans)-1]
+        curr := intervals[i]
+
+        if curr[0] <= last[1] {
+            if curr[1] > last[1] {
+                ans[len(ans)-1][1] = curr[1]
+            }
+        } else {
+            ans = append(ans, curr)
+        }
+    }
+
+    return ans
+}
