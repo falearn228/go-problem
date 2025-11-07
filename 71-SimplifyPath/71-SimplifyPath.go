2713131b6cfaeaa22b9c9776763bfec55c4bd21e
falearn <127325061+falearn228@users.noreply.github.com>
2025-11-07 14:45:34 +0700

    Create 71-SimplifyPath

---
+++ 71-SimplifyPath/71-SimplifyPath.go
@@ -0,0 +1,23 @@
+// Last updated: 11/7/2025, 2:49:05 PM
+func simplifyPath(path string) string {
+	stack := make([]string, 0, len(path))
+	fields := strings.Split(path, "/")
+
+	for _, v := range fields {
+		if v == "" {
+			continue
+		}
+
+		switch v {
+		case ".":
+		case "..":
+			if len(stack) > 0 {
+				stack = stack[:len(stack)-1]
+			}
+        default:
+		    stack = append(stack,v)
+		}
+	}
+
+	return "/" + strings.Join(stack, "/")
+}
