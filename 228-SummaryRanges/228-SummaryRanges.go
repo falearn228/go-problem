8a35515fe867a5f2324ccd2e8f68feab95c74afc
falearn <127325061+falearn228@users.noreply.github.com>
2025-11-07 14:44:42 +0700

    Create 228-SummaryRanges

---
+++ 228-SummaryRanges/228-SummaryRanges.go
@@ -0,0 +1,24 @@
+// Last updated: 11/7/2025, 2:48:13 PM
+func summaryRanges(nums []int) []string {
+	res := make([]string, 0, len(nums))
+
+	for i := 0; i < len(nums); i++ {
+		j := i
+		for ; j < len(nums)-1 && nums[j+1] == nums[j]+1; j++ {
+		}
+
+		if j == i {
+			start := strconv.Itoa(nums[j])
+			res = append(res, start)
+		} else {
+			start := strconv.Itoa(nums[i])
+			end := strconv.Itoa(nums[j])
+
+			res = append(res, start+"->"+end)
+			i = j
+		}
+
+	}
+
+	return res
+}
