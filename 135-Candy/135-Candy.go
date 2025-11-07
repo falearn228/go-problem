d18a3a2ae1301d28e1b9e3e099df2385198a6f0a
falearn <127325061+falearn228@users.noreply.github.com>
2025-11-07 14:45:19 +0700

    Create 135-Candy

---
+++ 135-Candy/135-Candy.go
@@ -0,0 +1,28 @@
+// Last updated: 11/7/2025, 2:48:50 PM
+func candy(ratings []int) int {
+    n := len(ratings)
+    if n == 1 {
+        return 1
+    }
+    
+    candies := make([]int, n)
+
+    for i := range candies {
+        candies[i] = 1
+        if i > 0 && ratings[i-1] < ratings[i] {
+            candies[i] += candies[i-1]
+        }
+    }
+
+    totalCandies, rightCandies := 0, 0
+    for i := n-1; i >= 0; i-- {
+        if i < n-1 && ratings[i+1] < ratings[i] {
+            rightCandies++
+        } else {
+            rightCandies = 1
+        }
+
+        totalCandies += max(candies[i], rightCandies)
+    }
+    return totalCandies
+}
