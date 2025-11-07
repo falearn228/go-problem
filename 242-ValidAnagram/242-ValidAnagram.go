e36cc46af4e2f34b97c44e793461e3c0da3606a8
falearn <127325061+falearn228@users.noreply.github.com>
2025-11-07 14:44:39 +0700

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/242-ValidAnagram.go
@@ -0,0 +1,19 @@
+// Last updated: 11/7/2025, 2:48:10 PM
+func isAnagram(s string, t string) bool {
+    if len(s) != len(t) {
+        return false
+    }
+    charCount := make(map[byte]int)
+    for i := range s {
+        charCount[s[i]]++
+    }
+
+    for i := range t {
+        cnt, ok := charCount[t[i]]
+        if !ok || cnt < 1 {
+            return false
+        }
+        charCount[t[i]]--
+    }
+    return true
+}
