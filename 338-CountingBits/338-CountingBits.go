e3cf276732c98f6cd732a1fb6d9e76adb060896b
falearn <127325061+falearn228@users.noreply.github.com>
2025-11-07 14:44:31 +0700

    Create 338-CountingBits

---
+++ 338-CountingBits/338-CountingBits.go
@@ -0,0 +1,11 @@
+// Last updated: 11/7/2025, 2:48:02 PM
+
+// Для заданного целого числа n верните массив ans длиной n + 1, в котором для каждого i (0 <= i <= n) ans[i] — это количество единиц в двоичном представлении i.
+func countBits(n int) []int {
+    ans := make([]int, n+1)
+    
+    for i := range ans {
+        ans[i] = ans[i >> 1] + (i & 1)
+    }
+    return ans
+}
