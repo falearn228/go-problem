657e4ae21f1963d128cc9e04f326ee8fe2a7b68a
falearn <127325061+falearn228@users.noreply.github.com>
2025-11-07 14:45:44 +0700

    Create 49-GroupAnagrams

---
+++ 49-GroupAnagrams/49-GroupAnagrams.go
@@ -0,0 +1,28 @@
+// Last updated: 11/7/2025, 2:49:14 PM
+func groupAnagrams(strs []string) [][]string {
+    if len(strs) == 0 {
+        return [][]string{}
+    }
+
+    anagrams := make(map[string][]string)
+    for _, s := range strs {
+        freq := make([]int, 26)
+        for _ ,ch := range s {
+            freq[ch-'a']++
+        }
+
+        var keyBd strings.Builder
+        for _, count := range freq {
+            keyBd.WriteByte('#')
+            keyBd.WriteString(strconv.Itoa(count))
+        } 
+        key := keyBd.String()
+        anagrams[key] = append(anagrams[key], s)
+    }
+
+    res := make([][]string, 0, len(anagrams))
+    for _, group := range anagrams {
+        res = append(res, group)
+    }
+    return res
+}
