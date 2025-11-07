fac9532fb8a5e729bd15bca2746da9dbec40c2c0
falearn <127325061+falearn228@users.noreply.github.com>
2025-11-07 14:44:30 +0700

    Create 345-ReverseVowelsOfAString

---
+++ 345-ReverseVowelsOfAString/345-ReverseVowelsOfAString.go
@@ -0,0 +1,33 @@
+// Last updated: 11/7/2025, 2:48:01 PM
+func reverseVowels(s string) string {
+    if len(s) == 1 {
+        return s
+    }
+
+	i, j := 0, len(s)-1
+
+	vowels := []byte{'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'}
+
+	result := make([]byte, len(s))
+	for i <= j {
+		if !slices.Contains(vowels, s[i]) {
+			result[i] = s[i]
+			i++
+		}
+		if !slices.Contains(vowels, s[j]) {
+			result[j] = s[j]
+			j--
+		}
+		if slices.Contains(vowels, s[i]) && slices.Contains(vowels, s[j]) {
+			result[i] = s[j]
+			result[j] = s[i]
+			fmt.Println(string(s[j]), string(s[i]))
+			i++
+			j--
+
+		}
+
+	}
+
+	return string(result)
+}
