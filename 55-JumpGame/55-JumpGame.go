67ad80f0b29f310df7f74581e186dec7a9e0488c
falearn <127325061+falearn228@users.noreply.github.com>
2025-11-07 14:45:41 +0700

    Create 55-JumpGame

---
+++ 55-JumpGame/55-JumpGame.go
@@ -0,0 +1,21 @@
+// Last updated: 11/7/2025, 2:49:11 PM
+func canJump(nums []int) bool {
+    n := len(nums)
+    if n == 1 {
+        return true
+    }
+
+    var maxReach int
+    for i, jump := range nums {
+        if maxReach >= n-1  {
+            return true
+        }
+        if i == n-1 || i > maxReach {
+            return false
+        }
+        maxReach = max(maxReach, i + jump)
+    }
+
+
+    return true
+}
