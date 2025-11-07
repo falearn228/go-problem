803ce79a3f1667acec8b10d6f72c5bfdb426ccb6
falearn <127325061+falearn228@users.noreply.github.com>
2025-11-07 14:44:18 +0700

    Create 450-DeleteNodeInABst

---
+++ 450-DeleteNodeInABst/450-DeleteNodeInABst.go
@@ -0,0 +1,38 @@
+// Last updated: 11/7/2025, 2:47:49 PM
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func deleteNode(root *TreeNode, key int) *TreeNode {
+    if root == nil {
+        return root
+    }
+
+    if key < root.Val {
+        root.Left = deleteNode(root.Left, key)
+        return root
+    } else if key > root.Val {
+        root.Right = deleteNode(root.Right, key)
+        return root
+    }
+
+    // key == root.Val
+    if root.Left == nil {
+        return root.Right
+    }
+    if root.Right == nil {
+        return root.Left
+    }
+
+    maxNode := root.Left
+    for maxNode.Right != nil {
+        maxNode = maxNode.Right
+    }
+    root.Val = maxNode.Val
+    root.Left = deleteNode(root.Left, maxNode.Val)
+    return root
+}
