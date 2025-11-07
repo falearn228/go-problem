8f8aa93d96748d2f12df93758555a3082ab10837
falearn <127325061+falearn228@users.noreply.github.com>
2025-11-07 14:45:59 +0700

    Create 21-MergeTwoSortedLists

---
+++ 21-MergeTwoSortedLists/21-MergeTwoSortedLists.cpp
@@ -0,0 +1,28 @@
+// Last updated: 11/7/2025, 2:49:29 PM
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode *dummy, *temp;
+        dummy = new ListNode();
+        temp = dummy;
+        
+        //when both list1 and list2 isn't empty
+        while(list1 && list2){
+            if(list1->val < list2->val){
+                temp->next = list1;
+                list1 = list1->next;
+            }
+            else{
+                temp->next = list2;
+                list2 = list2->next;   
+            }
+            temp = temp->next;
+        }
+        
+        // we reached at the end of one of the list
+        if(list1) temp->next = list1;
+        if(list2) temp->next = list2;
+        
+        return dummy->next;
+    }
+};
