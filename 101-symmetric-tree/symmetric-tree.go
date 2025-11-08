36b83bd08fd6e79e9423c7ebc554fe6d2f46aa42
falearn <127325061+falearn228@users.noreply.github.com>
2025-11-08 22:38:19 +0700

    Time: 0 ms (100.00%) | Memory: 4.9 MB (24.17%) - LeetSync

---
+++ 101-symmetric-tree/symmetric-tree.go
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isSymmetric(root *TreeNode) bool {
+    if root == nil {
+        return true
+    }
+
+    return isMirror(root.Left, root.Right)
+}
+
+func isMirror(l, r *TreeNode) bool {
+    if l == nil && r == nil {
+        return true
+    }
+    if l == nil || r == nil {
+        return false
+    }
+
+    if l.Val != r.Val {
+        return false
+    }
+    lr := isMirror(l.Left, r.Right)
+    rl := isMirror(l.Right, r.Left)
+    return lr && rl
+}
