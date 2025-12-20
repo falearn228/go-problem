720cbd96e507cbff30dea2dd0f018b5855e20ab4
falearn <127325061+falearn228@users.noreply.github.com>
2025-12-20 09:10:57 +0700

    Time: 2 ms (65.92%) | Memory: 4 MB (88.83%) - LeetSync

--- 97-interleaving-string/interleaving-string.go
+++ 97-interleaving-string/interleaving-string.go
@@ -1,27 +1,33 @@
 func isInterleave(s1 string, s2 string, s3 string) bool {
     n := len(s1)
     m := len(s2)
+    if n < m {
+        return isInterleave(s2,s1,s3)
+    }
     if n + m != len(s3) {
         return false
     }
 
-    dp := make([][]bool, n+1)
-    for i := range dp {
-        dp[i] = make([]bool, m+1)
-    }
-    // для пустых строк true
-    dp[0][0] = true
+    dp := make([]bool, m+1)
 
     for i := 0; i <= n; i++ {
         for j := 0; j <= m; j++ {
-            if i > 0 {
-                dp[i][j] = (dp[i-1][j] && s1[i-1] == s3[i+j-1])
+            if i == 0 && j == 0 {
+                dp[i] = true
+                continue
             }
-            if j > 0 && !dp[i][j] {
-                dp[i][j] = (dp[i][j-1] && s2[j-1] == s3[i+j-1])
+            if i == 0 {
+                // s2 берем, мы пришли слева, то есть по сути это новое значение
+                dp[j] = dp[j-1] && (s2[j-1] == s3[j-1]) 
+            } else if j == 0 {
+                // s1, мы пришли сверху, тк у нас одномерный массив, то ничего не изменится
+                // ну а если был бы двумерный, то стало типо dp[i-1][j]
+                dp[j] = dp[j] && (s1[i-1] == s3[i-1])
+            } else {
+                dp[j] = (dp[j-1] && s2[j-1] == s3[i+j-1]) || (dp[j] && s1[i-1] == s3[i+j-1])
             }
         }
     }
 
-    return dp[n][m]
+    return dp[m]
 }
